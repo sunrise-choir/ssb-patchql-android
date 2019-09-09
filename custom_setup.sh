e3e9dab642a9870001e39a19f0713b39e2750b46
Piet Geursen <pietgeursen@gmail.com>
2019-09-10 02:32:20 +1200

    ???

--- custom_setup.sh
+++ custom_setup.sh
@@ -1,7 +1,3 @@
 curl https://sh.rustup.rs -sSf | sh -s -- -y
 source $HOME/.cargo/env
 rustup target add aarch64-linux-android armv7-linux-androideabi i686-linux-android x86_64-linux-android
-export ORG_GRADLE_LOGGING_LEVEL=debug
-echo $NDK_HOME
-echo $NDK
-echo $ORG_GRADLE_LOGGING_LEVEL
