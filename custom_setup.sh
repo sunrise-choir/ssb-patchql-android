f16af305d9bf15f7fab748f1b3a9962f39926dd7
Piet Geursen <pietgeursen@gmail.com>
2019-09-10 03:19:44 +1200

    try use apt-get install

--- custom_setup.sh
+++ custom_setup.sh
@@ -1,3 +1,4 @@
+apt-get install build-essential cmake
 curl https://sh.rustup.rs -sSf | sh -s -- -y
 source $HOME/.cargo/env
 rustup target add aarch64-linux-android armv7-linux-androideabi i686-linux-android x86_64-linux-android
