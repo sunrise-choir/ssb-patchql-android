0963db35530ca1f148bfc7a19cbf0c5adf135a9e
Piet Geursen <pietgeursen@gmail.com>
2019-09-10 03:29:18 +1200

    typo

--- custom_setup.sh
+++ custom_setup.sh
@@ -1,4 +1,4 @@
-yes | $ANDROID_HOME/tools/bin/sdkmanager "cmake lldb ndk" && \
+yes | $ANDROID_HOME/tools/bin/sdkmanager "cmake lldb ndk-bundle" && \
 curl https://sh.rustup.rs -sSf | sh -s -- -y && \
 source $HOME/.cargo/env && \
 rustup target add aarch64-linux-android armv7-linux-androideabi i686-linux-android x86_64-linux-android
