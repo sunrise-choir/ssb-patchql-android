5f5df9702fb15193ccdfcd0277239066a823ccdf
Piet Geursen <pietgeursen@gmail.com>
2019-09-10 03:30:41 +1200

    dkjfdk

--- custom_setup.sh
+++ custom_setup.sh
@@ -1,4 +1,6 @@
-yes | $ANDROID_HOME/tools/bin/sdkmanager "cmake lldb ndk-bundle" && \
+yes | $ANDROID_HOME/tools/bin/sdkmanager "ndk-bundle" && \
+yes | $ANDROID_HOME/tools/bin/sdkmanager "lldb" && \
+yes | $ANDROID_HOME/tools/bin/sdkmanager "cmake" && \
 curl https://sh.rustup.rs -sSf | sh -s -- -y && \
 source $HOME/.cargo/env && \
 rustup target add aarch64-linux-android armv7-linux-androideabi i686-linux-android x86_64-linux-android
