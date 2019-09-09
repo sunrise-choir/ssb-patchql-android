2d8ce80a86a5ebda69d052b73979037da122cefa
Piet Geursen <pietgeursen@gmail.com>
2019-09-10 03:39:37 +1200

    specify ndk cmake lldb versions

--- custom_setup.sh
+++ custom_setup.sh
@@ -1,6 +1,4 @@
-yes | $ANDROID_HOME/tools/bin/sdkmanager "ndk-bundle" && \
-yes | $ANDROID_HOME/tools/bin/sdkmanager "LLDB" && \
-yes | $ANDROID_HOME/tools/bin/sdkmanager "CMake" && \
+$ANDROID_HOME/tools/bin/sdkmanager --install "lldb;3.1" "ndk;20.0.5594570" "cmake;3.10.2.4988404" && \
 curl https://sh.rustup.rs -sSf | sh -s -- -y && \
 source $HOME/.cargo/env && \
 rustup target add aarch64-linux-android armv7-linux-androideabi i686-linux-android x86_64-linux-android
