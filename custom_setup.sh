c55d864741588e492e56259f3a13e441d5e6428c
Piet Geursen <pietgeursen@gmail.com>
2019-09-10 01:42:08 +1200

    echo NDK_HOME

--- custom_setup.sh
+++ custom_setup.sh
@@ -1,3 +1,4 @@
 curl https://sh.rustup.rs -sSf | sh -s -- -y
 source $HOME/.cargo/env
 rustup target add aarch64-linux-android armv7-linux-androideabi i686-linux-android x86_64-linux-android
+echo $NDK_HOME
