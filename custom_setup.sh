014b400520f4e2825f002113523952435aa9edb4
Piet Geursen <pietgeursen@gmail.com>
2019-09-10 03:59:36 +1200

    do we have a cc

--- custom_setup.sh
+++ custom_setup.sh
@@ -2,3 +2,6 @@ $ANDROID_HOME/tools/bin/sdkmanager --install "lldb;3.1" "ndk;20.0.5594570" "cmak
 curl https://sh.rustup.rs -sSf | sh -s -- -y && \
 source $HOME/.cargo/env && \
 rustup target add aarch64-linux-android armv7-linux-androideabi i686-linux-android x86_64-linux-android
+echo "do we have a cc"
+which cc
+cc
