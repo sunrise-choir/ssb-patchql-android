1e1cfe0434c2f3b79c094ef59e08d854ccdd305f
Piet Geursen <pietgeursen@gmail.com>
2019-09-10 01:58:29 +1200

    Mayber verbose gradle builds

--- custom_setup.sh
+++ custom_setup.sh
@@ -1,4 +1,7 @@
 curl https://sh.rustup.rs -sSf | sh -s -- -y
 source $HOME/.cargo/env
 rustup target add aarch64-linux-android armv7-linux-androideabi i686-linux-android x86_64-linux-android
+export ORG_GRADLE_LOGGING_LEVEL=debug
 echo $NDK_HOME
+echo $NDK
+echo $ORG_GRADLE_LOGGING_LEVEL
