d4721903bb06692dc193d1479ee0390e78fa1c64
Piet Geursen <pietgeursen@gmail.com>
2019-09-10 03:27:24 +1200

    idunno

--- custom_setup.sh
+++ custom_setup.sh
@@ -1,4 +1,4 @@
-apt-get install build-essential cmake
-curl https://sh.rustup.rs -sSf | sh -s -- -y
-source $HOME/.cargo/env
+yes | $ANDROID_HOME/tools/bin/sdkmanager "cmake lldb ndk" && \
+curl https://sh.rustup.rs -sSf | sh -s -- -y && \
+source $HOME/.cargo/env && \
 rustup target add aarch64-linux-android armv7-linux-androideabi i686-linux-android x86_64-linux-android
