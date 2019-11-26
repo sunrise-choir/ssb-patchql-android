d4c913d741882db61ecd0052ba6764e42a1b2560
Piet Geursen <pietgeursen@gmail.com>
2019-11-26 16:14:41 +0100

    Tidy up a little

--- custom_setup.sh
+++
@@ -1,7 +0,0 @@
-$ANDROID_HOME/tools/bin/sdkmanager --install "lldb;3.1" "ndk;20.0.5594570" "cmake;3.10.2.4988404" && \
-curl https://sh.rustup.rs -sSf | sh -s -- -y && \
-source $HOME/.cargo/env && \
-rustup target add aarch64-linux-android armv7-linux-androideabi i686-linux-android x86_64-linux-android
-echo "do we have a cc"
-which cc
-cc
